b85f0f29d3f4e381df61d9bfbfd1e24ba2538cba
Drackow1 <avictor0.2@gmail.com>
2025-04-07 08:56:44 -0300

    testando

---
+++ Weak Typing.js
@@ -0,0 +1,13 @@
+let  any = 'Amazing'
+console.log(any)
+console.log (typeof any)
+
+any = 3.1345
+console.log (any)
+console.log (typeof any)
+
+// Avoid these words
+
+let  number
+let valor
+let pqp  =false // Chemistral Product
